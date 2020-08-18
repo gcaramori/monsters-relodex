aa4dc32aa783bde6f45c1537f0eee9d7b805473c
gcaramori <gcaramori@outlook.com>
2020-08-18 15:09:10 -0300

    Updates

---
+++ precache-manifest.77a82a3b776658ba0796ca08c6c753d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc4f24e6efcb7b6a0a00704b7bd6716",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "07148ed8b1ec0832818f",
+    "url": "/monsters-relodex/static/css/main.64dc121f.chunk.css"
+  },
+  {
+    "revision": "5db257ba903a8fee2e3c",
+    "url": "/monsters-relodex/static/js/2.c75ba697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-relodex/static/js/2.c75ba697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07148ed8b1ec0832818f",
+    "url": "/monsters-relodex/static/js/main.f9e824c1.chunk.js"
+  },
+  {
+    "revision": "858dbcd1760996d62d88",
+    "url": "/monsters-relodex/static/js/runtime-main.a1871825.js"
+  }
+]);
